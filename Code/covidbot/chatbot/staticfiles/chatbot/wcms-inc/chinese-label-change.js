9526054a29e98ddca2cc30cf16f5bae6719a3dc0
vishalpadma17 <vishalpadma17@gmail.com>
2020-07-29 21:57:27 +0100

    Revert "Merge branch 'master' of https://github.com/ucd-nlmsc-teamproject/TeamRocket"
    This reverts commit ce1a298da1e746fa9533fc5579df2471622cd50e, reversing
    changes made to bd88050fa9b68252efca8eb7f3e5475bbe2f81ea.

--- Code/covidbot/chatbot/staticfiles/chatbot/wcms-inc/chinese-label-change.js
+++
@@ -1,9 +0,0 @@
-/* Raw JS */
-$(document).ready(function() {
-    $('a.dropdown-item.fs08').each(function() {
-        if ($(this).html() == '繁體中文') {
-            $(this).html('简体中文');
-        }
-    });
-});
-/* End Raw JS */
