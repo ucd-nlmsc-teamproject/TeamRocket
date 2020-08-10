e6052e13a6d23369198ad10d04878ce1e129e077
nripendra-singh <nripendra91@gmail.com>
2020-08-10 10:58:54 +0100

    Collect Static pre deployment
    Co-Authored-By: sumitkrs99 <58439903+sumitkrs99@users.noreply.github.com>

--- CovidChatBot/covidbot/chatbot/views.py
+++ CovidChatBot/covidbot/chatbot/views.py
@@ -1,6 +1,4 @@
 from django.shortcuts import render
-from bs4 import BeautifulSoup
-import requests
 
 # Create your views here.
 def home(request):
@@ -19,8 +17,4 @@ def footer(request):
     return render(request,'chatbot/footer.html')
 
 def indexie(request):
-    url = 'https://www.irishtimes.com'
-    soup = requests.get(url + '/news/health/coronavirus')
-    soup = BeautifulSoup(soup.text)
-    headlines = {hl.find('span', class_='h2').text:url + hl['href'] for hl in soup.find_all('a', class_='gtm-event')[4:5]}
-    return render(request,'chatbot/indexie.html', {'headlines': headlines})
+    return render(request,'chatbot/indexie.html')
